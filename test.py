1a92bf7160c7a113304d1494d2e5fa72e2dacaf2
sanek1988 <sanekwell@mail.ru>
2022-05-09 12:42:31 +0300

    новое

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
-1212323143
-sadfqf
-sfsf
+from setuptools import setup
+
+setup(
+    name="pyneng",
+    version="1.9",
+    py_modules=["pyneng"],
+    install_requires=[
+        "Click",
+        "pyyaml",
+        "pytest",
+        "pytest-clarity",
+        "pytest-json-report",
+        "requests",
+        "PyGithub",
+        "six",
+        "jinja2",
+        "textfsm",
+    ],
+    entry_points="""
+        [console_scripts]
+        pyneng=pyneng:cli
+    """,
+)
